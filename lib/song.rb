64e90075c35ad5fa47ee02b9157c0982a485fdc3
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-22 20:19:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,6 +38,16 @@ class Song
   end
   
   def artist_count
-    
+    def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
   end
 end
